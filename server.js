94489cab77e12c2c04bd98fd07289d7e7034eb75
Abhishek Singh <Abhishek Singh>
2016-10-22 18:34:05 +0530

    login form

--- server.js
+++ server.js
@@ -113,7 +113,9 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        self.app = express.createServer();
+        //self.app = express.createServer();
+        self.app = app;
+        self.app.use(express.static(__dirname));
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
@@ -156,4 +158,3 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
-
